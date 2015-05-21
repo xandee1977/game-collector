81d955b2d28f2d46dd0c01e0abb2d4721b5920d8
Alexandre Conrado <alexandre.conrado@corp.terra.com.br>
2015-05-21 13:59:33 -0300

    Ajuste controller

--- game-collector/game-collector-app/www/js/GameApp.js
+++ game-collector/game-collector-app/www/js/GameApp.js
@@ -3,39 +3,3 @@ GameApp.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
-
-GameApp.factory('loadList', [ '$http', '$q', 
-    function( $http, $q ) {
-
-        var pub = {};
-
-        var jsonUrl = 'http://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/warriors/json/json-as2015.js?callback=JSON_CALLBACK' + (new Date().getTime()),
-            cachedResponse;
-
-        pub.getEvent = function() {
-
-            var deferred = $q.defer();
-
-            if ( cachedResponse ) {
-                deferred.resolve( cachedResponse );
-            }
-
-            else {
-
-                $http.jsonp( jsonUrl );
-
-                window.jsonpCallbackAllStar2015 = function( data ) {
-                    cachedResponse = data;
-                    deferred.resolve( data );
-                }
-
-            }
-
-            return deferred.promise;
-
-        };
-
-        return pub;
-
-    }
-]);
--- game-collector/game-collector-app/www/js/GameController.js
+++ game-collector/game-collector-app/www/js/GameController.js
@@ -1,6 +1,48 @@
 (function() {
      GameApp.controller('GameController', ['$scope','$http', function($scope,$http) {
-          //$http is working in this
-        this.gameList = window.listGames;      
+        $scope.teste = 'LERO LERO';
+        $scope.gameList = [
+            {
+                "game_id":"1",
+                "game_title":"2020 Super Baseball",
+                "game_desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultrices, nulla vel venenatis euismod, quam dui auctor enim, ut egestas risus enim et felis.",
+                "game_developer":null,
+                "game_type_id":null,
+                "system_id":"1"
+            }
+                
+            ,    {
+                "game_id":"2",
+                "game_title":"3 Ninjas Kick Back",
+                "game_desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultrices, nulla vel venenatis euismod, quam dui auctor enim, ut egestas risus enim et felis.",
+                "game_developer":null,
+                "game_type_id":null,
+                "system_id":"1"
+            }
+                
+            ,    {
+                "game_id":"3",
+                "game_title":"3x3 Eyes - Seima Kourinden",
+                "game_desc":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultrices, nulla vel venenatis euismod, quam dui auctor enim, ut egestas risus enim et felis.",
+                "game_developer":null,
+                "game_type_id":null,
+                "system_id":"1"
+            }
+        ];
+
+        $scope.getData = function() {
+            /*
+            var initInjector = angular.injector(["ng"]);
+            var $http = initInjector.get("$http");
+            
+            return $http.get("http://beecoapp.com/ws-game/?service=game&action=list").then(function(response) {
+                //myApplication.constant("config", response.data);
+                console.log(response.data);
+            }, function(errorResponse) {
+                // Handle error case
+            });
+            */
+            console.log("teste");
+        };
      }]);
 })();
