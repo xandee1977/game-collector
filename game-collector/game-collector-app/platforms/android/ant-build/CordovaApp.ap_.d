a79a5f68aa96d7ac78439fb4b2e872cab500db58
Alexandre Conrado <xandee1977@gmail.com>
2015-06-04 18:49:08 -0300

    ajustes

--- game-collector/game-collector-app/platforms/android/ant-build/CordovaApp.ap_.d
+++ game-collector/game-collector-app/platforms/android/ant-build/CordovaApp.ap_.d
@@ -3,11 +3,7 @@
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/values/strings.xml \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-port-xhdpi/screen.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-land-xhdpi/screen.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-xhdpi/icon.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-hdpi/icon.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-port-hdpi/screen.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-ldpi/icon.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-mdpi/icon.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-land-hdpi/screen.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable-land-ldpi/screen.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/res/drawable/icon.png \
@@ -17,11 +13,7 @@
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-port-xhdpi/screen.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-land-xhdpi/screen.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-hdpi/icon.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-ldpi/icon.png \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-mdpi/icon.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
 /home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/res/drawable/icon.png \
