9ec73012324832c476b57a099583f89f737f3830
Alexandre Conrado <alexandre.conrado@corp.terra.com.br>
2015-06-05 08:26:36 -0300

    Ajustes

--- game-collector/game-collector-app/platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
+++ game-collector/game-collector-app/platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -1,4 +1,4 @@
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/CordovaApp.ap_ \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/ant-build/classes.dex \
-/home/xandee1977/projetos/apache/game-collector/game-collector/game-collector-app/platforms/android/CordovaLib/ant-build/classes.jar \
+/home/conrado/git/conrado/game-collector/game-collector/game-collector-app/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/home/conrado/git/conrado/game-collector/game-collector/game-collector-app/platforms/android/ant-build/CordovaApp.ap_ \
+/home/conrado/git/conrado/game-collector/game-collector/game-collector-app/platforms/android/ant-build/classes.dex \
+/home/conrado/git/conrado/game-collector/game-collector/game-collector-app/platforms/android/CordovaLib/ant-build/classes.jar \
